061894d910f7a777a2644b0b673d482a2e035f6e
Nementon <gueux666@gmail.com>
2015-04-05 20:10:04 +0200

    Improve IWorld | Add I2DWorld

--- core/src/fr/n7/game/flappyzombie/fr/n7/game/flappyzombie/fr/n7/game/flappyzombie/world/I2DWorld.java
+++ core/src/fr/n7/game/flappyzombie/fr/n7/game/flappyzombie/fr/n7/game/flappyzombie/world/I2DWorld.java
@@ -1,14 +1,13 @@
 package fr.n7.game.flappyzombie.fr.n7.game.flappyzombie.fr.n7.game.flappyzombie.world;
 
+import com.badlogic.gdx.math.Shape2D;
+
+import java.util.List;
+
 /**
  * Created by Nementon on 05/04/2015.
  */
-public interface IWorld {
-    void update(float delta);
+public interface I2DWorld extends IWorld {
 
-    int width();
-
-    int height();
-
-    int bounds();
+    List<Shape2D> shapes();
 }
